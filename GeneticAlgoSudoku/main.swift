9667d0b547d7e379e4743199c6b0a9c817bdcac9
Mounir Ybanez <rinuom91@gmail.com>
2017-04-06 13:57:11 +0800

    Initial commit.

---
+++ GeneticAlgoSudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GeneticAlgoSudoku
+//
+//  Created by Mounir Ybanez on 06/04/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

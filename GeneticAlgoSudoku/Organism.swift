f88290521c92f99bdc636964c8a3839909ceb4b1
Mounir Ybanez <rinuom91@gmail.com>
2017-04-10 16:12:43 +0800

    Implemented CustomStringConvertible.

--- GeneticAlgoSudoku/Organism.swift
+++ GeneticAlgoSudoku/Organism.swift
@@ -29,3 +29,26 @@ public func ==(lhs: Organism, rhs: Organism) -> Bool {
     return lhs.score == rhs.score && lhs.chromosomes == rhs.chromosomes
 }
 
+extension Organism: CustomStringConvertible {
+    
+    public var description: String {
+        var description = "\n"
+        for (index, chromosome) in chromosomes.enumerated() {
+            if index % 3 == 0 && index % 9 != 0 {
+                description.append(" | ")
+            }
+            
+            if index != 0 && index % 9 == 0 {
+                description.append("\n")
+            }
+            
+            if index != 0 && index % 27 == 0 {
+                description.append("---------------------------------\n")
+            }
+            
+            description.append(" \(chromosome.data) ")
+        }
+        description.append("\n")
+        return description
+    }
+}

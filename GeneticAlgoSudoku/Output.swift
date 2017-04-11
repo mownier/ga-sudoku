591ac17d46600ef616c0e5f7935b81ec9bccdd20
Mounir Ybanez <rinuom91@gmail.com>
2017-04-11 15:00:59 +0800

    Implemented didUpdatePopulation.

--- GeneticAlgoSudoku/Output.swift
+++ GeneticAlgoSudoku/Output.swift
@@ -6,23 +6,26 @@
 //  Copyright © 2017 Ner. All rights reserved.
 //
 
-public struct Output: GeneticAlgorithmOutputProtocol {
+public struct TerminalOutput: GeneticAlgorithmOutputProtocol {
 
     public func didComplete(_ result: GeneticAlgorithmResult) {
         switch result {
         case .fittestFound(let generation, let organisms):
             print("Solution found")
             print("On generation:", generation)
-            for organism in organisms {
-                print(organism)
-            }
+            organisms.forEach({ print($0) })
             
         case .generationFinished(let organisms):
             print("Solution not found")
-            for organism in organisms {
-                print("\nScore:", organism.score)
-                print(organism)
-            }
+            organisms.sorted(by: { $0.score < $1.score }).forEach({
+                print("\nScore:", $0.score)
+                print($0)
+            })
         }
+    }
+    
+    public func didUpdatePopulation(_ generation: Int, bestOrganisms: [Organism]) {
+        print("Generation: \(generation),", "Best:", bestOrganisms[0].score)
+        bestOrganisms.forEach({ print($0) })
     }
 }
